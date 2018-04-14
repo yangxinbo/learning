b4eb567ccf9c438d8a6f72d9492c4705becc1a93
yangxinbo <yang.xinbo@vpclub.cn>
2018-04-15 02:17:35 +0800

    init

---
+++ src/main/java/com/lodumani/LodumaniApplication.java
@@ -0,0 +1,12 @@
+package com.lodumani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodumaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodumaniApplication.class, args);
+	}
+}
